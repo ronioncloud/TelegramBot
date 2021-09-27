a34a22aec645bdb34ef0fc85066890ef5b5aaee0
Yuu <83197226+oi-eu-sou-linux@users.noreply.github.com>
2021-09-27 00:24:06 -0300

    /gato
    Envia foto aleatória de gato

--- comandos-gerais/gato.py
+++ comandos-gerais/gato.py
@@ -3,3 +3,4 @@ def gatin(message):
     cat = requests.get('https://thatcopy.pw/catapi/rest')
     gato = cat.json()
     bot.send_photo(message.chat.id, gato['url'])
+    # Kkkk eu tava quebrando a cabeça pra fazer download da url, sendo que poderia somente incorporar a URL :D
