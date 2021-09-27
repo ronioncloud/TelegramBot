57111db77338250fe8c32c3d8216464204af0ef1
Yuu <83197226+oi-eu-sou-linux@users.noreply.github.com>
2021-09-27 00:22:02 -0300

    Update gato.py

--- comandos-gerais/gato.py
+++ comandos-gerais/gato.py
@@ -1,11 +1,5 @@
-import requests
-import telebot
-bot = telebot.TeleBot('TOKEN')
-
 @bot.message_handler(commands=['gato'])
 def gatin(message):
     cat = requests.get('https://thatcopy.pw/catapi/rest')
     gato = cat.json()
     bot.send_photo(message.chat.id, gato['url'])
-
-bot.polling(non_stop=True)
